e7e4010858a21d5835d046a0c6c0a53593144c2e
Linda Hrdinová <hrlin@seznam.cz>
2023-11-18 14:15:47 +0100

    vytvoření OrderForm komponenty

--- src/components/ChosenRoom/index.jsx
+++ src/components/ChosenRoom/index.jsx
@@ -1,4 +1,5 @@
 import './style.css';
+import { OrderForm } from '../OrderForm';
 
 export const ChosenRoom = () => {
   return (
@@ -15,54 +16,7 @@ export const ChosenRoom = () => {
               saepe. Vero.
             </p>
           </div>
-          <form>
-            <div className="form-fields">
-              <label htmlFor="dateFrom" className="field-label">
-                Od:
-              </label>
-              <input id="dateFrom" className="field-input" type="date" />
-              <label htmlFor="dateTo" className="field-label">
-                Do:
-              </label>
-              <input id="howManyPersons" className="field-input" type="date" />
-              <label htmlFor="howManyPersons" className="field-label">
-                Počet osob:
-              </label>
-              <input id="dateTo" className="field-input" type="text" />
-              <label htmlFor="select" className="field-label">
-                Stravování:
-              </label>
-              <select id="select" className="field-input">
-                <option>vyberte</option>
-                <option>Snídaně</option>
-                <option>Polopenze</option>
-                <option>Plná penze</option>
-              </select>
-
-              <label htmlFor="check1" className="field-label">
-                Domácí mazlíček:
-              </label>
-              <input id="check1" className="field-input" type="checkbox" />
-              <label htmlFor="check1" className="field-label">
-                Přistýlka pro dítě:
-              </label>
-              <input id="check1" className="field-input" type="checkbox" />
-              <label htmlFor="check1" className="field-label">
-                Bezbariérový přístup:
-              </label>
-              <input id="check1" className="field-input" type="checkbox" />
-              <label htmlFor="howManyPersons" className="field-label">
-                Email:
-              </label>
-              <input id="dateTo" className="field-input" type="email" />
-              <label htmlFor="howManyPersons" className="field-label">
-                Telefon:
-              </label>
-              <input id="dateTo" className="field-input" type="tel" />
-            </div>
-            <p>Celková cena za pobyt: Kč</p>
-            <button className="wide">Odeslat poptávku</button>
-          </form>
+          <OrderForm />
         </div>
       </div>
     </section>
