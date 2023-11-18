270020eb9f57264408249b8ec2aa325903388624
Lucie Dušková <lucie.duskova87@gmail.com>
2023-11-18 12:24:12 +0100

    Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,14 +5,13 @@ export const Footer = () => {
     <section className="dark">
       <div className="container columns-2">
         <div className="columns">
-          <h2>Heading</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
-            mollitia ut pariatur eos error sunt blanditiis repellat similique
-            aperiam temporibus.
-          </p>
+          <h2>Kontakt</h2>
+          <p className='footer_adress'>Hotel Stříbrava</p>
+          <p className='footer_adress'>Ke Kamenné lávce 12</p>
+          <p className='footer_adress'>317 24  Libnice nad Stříbravou</p>
+          <a className='footer_email' href="mailto:recepce@hotelstribrava.cz">recepce@hotelstribrava.cz</a>
         </div>
-        <img src="img/image1.svg" />
+        <img src="./img/mapa.png" alt="mapa"/>
       </div>
     </section>
   );
