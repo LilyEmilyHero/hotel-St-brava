d88f3d9e1212c948cbd74617ae15f9aae8f69b5a
Lucie Dušková <lucie.duskova87@gmail.com>
2023-11-18 11:29:17 +0100

    prejmenování n aclassName

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,13 @@
 import { ChosenRoom } from '../../components/ChosenRoom';
 import { Footer } from '../../components/Footer';
-import { Header } from '../../components/Header';
+import { Banner } from '../../components/Banner';
 import { Rooms } from '../../components/Rooms';
 import './style.css';
 
 export const HomePage = () => {
   return (
     <>
-      <Header />
+      <Banner />
       <Rooms />
       <ChosenRoom />
       <Footer />
